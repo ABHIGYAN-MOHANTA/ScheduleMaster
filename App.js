2825e38e8ef9defe6291f6604d8f0b0ff8ee8857
Abhigyan Mohanta <110360901+ABHIGYAN-MOHANTA@users.noreply.github.com>
2023-09-01 00:26:27 +0530

    ScheduleMaster init

--- App.js
+++ App.js
@@ -1,20 +1,124 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
+import { Fontisto } from "@expo/vector-icons";
+import { SimpleLineIcons } from "@expo/vector-icons";
+
+import Daily from "./screens/Daily";
+import Monthly from "./screens/Monthly";
+import Yearly from "./screens/Yearly";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <StatusBar hidden />
+        <Text style={styles.title}>ScheduleMaster</Text>
+        <Text style={styles.headerMiddle}>
+          Make a Plan(Don't Wake up like an Accident)
+        </Text>
+        <Text style={styles.header}>
+          Manage Frustrations and Minimize Regrets when you sometimes fail to
+          meet the Schedule!T=0
+        </Text>
+        <NavigationContainer>
+          <Tab.Navigator
+            screenOptions={{
+              tabBarActiveTintColor: "#1E1E1E",
+              tabBarInactiveTintColor: "#D3D3D3",
+              tabBarActiveBackgroundColor: "#D3D3D3",
+              tabBarInactiveBackgroundColor: "#1E1E1E",
+              headerStyle: {
+                backgroundColor: "#1E1E1E",
+              },
+              headerTitleStFocusedyle: {
+                color: "#D3D3D3",
+                fontWeight: "bold",
+                fontSize: 25,
+              },
+            }}
+          >
+            <Tab.Screen
+              name="Daily"
+              component={Daily}
+              options={{
+                tabBarIcon: ({ focused }) => (
+                  <Fontisto
+                    name="day-haze"
+                    size={24}
+                    color={focused ? "#1E1E1E" : "#D3D3D3"}
+                  />
+                ),
+                headerShown: false,
+              }}
+            />
+            <Tab.Screen
+              name="Monthly"
+              component={Monthly}
+              options={{
+                tabBarIcon: ({ focused }) => (
+                  <FontAwesome
+                    name="calendar"
+                    size={22}
+                    color={focused ? "#1E1E1E" : "#D3D3D3"}
+                  />
+                ),
+                headerShown: false,
+              }}
+            />
+            <Tab.Screen
+              name="Yearly"
+              component={Yearly}
+              options={{
+                tabBarIcon: ({ focused }) => (
+                  <SimpleLineIcons
+                    name="globe-alt"
+                    size={24}
+                    color={focused ? "#1E1E1E" : "#D3D3D3"}
+                  />
+                ),
+                headerShown: false,
+              }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  header: {
+    backgroundColor: "#1E1E1E",
+    textAlign: "center",
+    fontStyle: "italic",
+    color: "#D3D3D3",
+    paddingBottom: 20,
+  },
+  headerMiddle: {
+    backgroundColor: "#1E1E1E",
+    textAlign: "center",
+    fontVariant: "bold",
+    color: "#D3D3D3",
+    paddingBottom: 2,
+  },
+  title: {
+    backgroundColor: "#1E1E1E",
+    textAlign: "center",
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "#D3D3D3",
+    paddingTop: 30,
   },
 });
---
+++ screens/Monthly.js
@@ -0,0 +1,208 @@
+import React, { useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Ionicons } from "@expo/vector-icons";
+
+const Monthly = () => {
+  const [eventTitle, setEventTitle] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [monthlyEvents, setMonthlyEvents] = useState([]);
+
+  useEffect(() => {
+    loadMonthlyEvents();
+  }, []);
+
+  useEffect(() => {
+    saveMonthlyEvents();
+  }, [monthlyEvents]);
+
+  const loadMonthlyEvents = async () => {
+    try {
+      const storedMonthlyEvents = await AsyncStorage.getItem("monthlyEvents");
+      if (storedMonthlyEvents) {
+        setMonthlyEvents(JSON.parse(storedMonthlyEvents));
+      }
+    } catch (error) {
+      console.error("Error loading monthlyEvents:", error);
+    }
+  };
+
+  const saveMonthlyEvents = async () => {
+    try {
+      await AsyncStorage.setItem(
+        "monthlyEvents",
+        JSON.stringify(monthlyEvents)
+      );
+    } catch (error) {
+      console.error("Error saving monthlyEvents:", error);
+    }
+  };
+
+  const handleAddEvent = () => {
+    if (
+      eventTitle.trim() === "" ||
+      startTime.trim() === "" ||
+      endTime.trim() === ""
+    ) {
+      return;
+    }
+    const newEvent = {
+      id: Date.now().toString(),
+      title: eventTitle,
+      startTime,
+      endTime,
+    };
+    setMonthlyEvents([...monthlyEvents, newEvent]);
+    setEventTitle("");
+    setStartTime("");
+    setEndTime("");
+  };
+
+  const handleDeleteEvent = (id) => {
+    const updatedMonthlyEvents = monthlyEvents.filter(
+      (event) => event.id !== id
+    );
+    setMonthlyEvents(updatedMonthlyEvents);
+  };
+
+  const handleDeleteAllMonthlyEvents = () => {
+    setMonthlyEvents([]);
+    AsyncStorage.removeItem("monthlyEvents");
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Event Title"
+          value={eventTitle}
+          onChangeText={setEventTitle}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Start Time (e.g., 4:00 AM)"
+          value={startTime}
+          onChangeText={setStartTime}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="End Time (e.g., 5:30 AM)"
+          value={endTime}
+          onChangeText={setEndTime}
+        />
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.addButton} onPress={handleAddEvent}>
+            <Text style={styles.addButtonText}>Add Event</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.deleteAllButton}
+            onPress={handleDeleteAllMonthlyEvents}
+          >
+            <Text style={styles.deleteAllButtonText}>Clear All</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <FlatList
+        data={monthlyEvents}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View style={styles.eventItem}>
+            <View style={styles.eventInfo}>
+              <Text style={styles.eventTitle} numberOfLines={1}>
+                {item.title}
+              </Text>
+              <Text style={styles.eventTime}>
+                {item.startTime} - {item.endTime}
+              </Text>
+            </View>
+            <TouchableOpacity onPress={() => handleDeleteEvent(item.id)}>
+              <Ionicons name="ios-trash" size={24} color="#FF0000" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FF6F61",
+    padding: 20,
+  },
+  inputContainer: {
+    marginBottom: 20,
+  },
+  input: {
+    width: "100%",
+    padding: 12,
+    marginBottom: 10,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 8,
+    color: "#333",
+  },
+  addButton: {
+    backgroundColor: "#007AFF",
+    paddingVertical: 14,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  addButtonText: {
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  deleteAllButton: {
+    backgroundColor: "#FF0000",
+    paddingVertical: 14,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  deleteAllButtonText: {
+    color: "#FFFFFF",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+  eventItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+    borderRadius: 8,
+    padding: 16,
+    marginBottom: 10,
+  },
+  eventInfo: {
+    flex: 1,
+    marginRight: 10,
+  },
+  eventTitle: {
+    fontSize: 18,
+    color: "#333",
+    fontWeight: "bold",
+  },
+  eventTime: {
+    fontSize: 16,
+    color: "#666",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 20,
+  },
+});
+
+export default Monthly;
